4d0ce9d96de4ffede0bc519335bd390c3f9cc5e7
proybal <proybal@yahoo.com>
2022-05-21 08:11:50 -0600

    eigth time

--- news/models.py
+++ news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    source = models.URLField()
+    title = models.CharField(max_length=20)
+    cover = models.ImageField(upload_to='images/')
+    url = models.URLField()
+
+    def __str__(self):
+        return self.source
