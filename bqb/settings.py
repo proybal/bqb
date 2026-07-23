2d109867ff7c02992edfee9a46b0a73ce1e55da0
proybal9 <156149181+proybal9@users.noreply.github.com>
2026-07-23 11:10:04 -0600

    fixed timezone to America/Denver

--- bqb/settings.py
+++ bqb/settings.py
@@ -105,17 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
+LANGUAGE_CODE: str = 'en-us'
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Denver'
 
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
